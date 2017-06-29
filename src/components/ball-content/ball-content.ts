6d51eed63997302fbda78bfd0c534673021945cf
sjin <xiao_wen202129@163.com>
2017-06-29 16:52:33 +0800

    一两行的html代码就不需要单独的模板文件了

--- src/components/ball-content/ball-content.ts
+++ src/components/ball-content/ball-content.ts
@@ -3,11 +3,16 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'components-ball-content',
-  templateUrl: 'ball-content.html'
+  template: `<div class="components-ball-content">
+            <span *ngFor="let redBall of reds">{{redBall}}</span>
+            <span>{{blue}}</span>
+          </div>
+          `
 })
 export class BallContentComponent {
   @Input() reds;
   @Input() blue:string;
+
   constructor() {
   }
 }
